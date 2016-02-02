639c3004bc30543b4649f7acec11e83f541f9b03
MrLisongbo <1967375645@qq.com>
2016-02-02 13:31:12 +0800

    删除一行

--- src/main/java/com/spring/lisongbo/controller/FirstController.java
+++ src/main/java/com/spring/lisongbo/controller/FirstController.java
@@ -11,7 +11,6 @@ import com.spring.lisongbo.pojo.User;
 @RequestMapping(name = "firstcontroller/login")
 public class FirstController {
     
-    
     private String str;
     private int num;
     public String getStr() {
