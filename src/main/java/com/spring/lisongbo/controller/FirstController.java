f7becae2ec020118dd0c54015ac6aeb1d615cc80
lisongbo <1967375645@qq.com>
2016-02-02 11:32:01 +0800

    检查冲突如何解决

--- src/main/java/com/spring/lisongbo/controller/FirstController.java
+++ src/main/java/com/spring/lisongbo/controller/FirstController.java
@@ -1,5 +1,18 @@
 package com.spring.lisongbo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.spring.lisongbo.pojo.User;
+
+@Controller
+@RequestMapping(name="firstcontroller/login")
 public class FirstController {
 
+    @RequestMapping(name="op_user_login.jsp")
+    public String login( @Validated @RequestBody User user){
+        return "home";
+    }
 }
